820688166d9035d4f3348d1ea1312bc90700844b
Warner Carpenter <warnercarpenter@yahoo.com>
2019-05-22 22:20:25 -0500

    technologies finished

--- src/components/blocks/Technologies.js
+++ src/components/blocks/Technologies.js
@@ -1,10 +1,25 @@
 import React, { Component } from 'react'
+import technologies from '../../data/Technologies';
 
 class Technologies extends Component {
 
     render() {
         return (
-            <div id="technologies" className="technologies"></div>
+            <div id="technologies" className="technologies">
+                <h1 className="lightBgTitle">Technologies</h1>
+
+                <div className="technologyCardContainer">
+                    {
+                        technologies.map((technology, i) => {
+                            return <a key={i} className="technologyCard" href={technology.link} target="_blank" rel="noopener noreferrer">
+                                <div className={technology.className + " technologyImage"}></div>
+                                <div className="technologyText">{technology.name}</div>
+                            </a>
+                        })
+                    }
+                </div>
+
+            </div>
         )
     }
 }
---
+++ src/data/Technologies.js
@@ -0,0 +1,90 @@
+
+ const technologies = [
+    {
+        "name": "Javascript",
+        "className": "javascript",
+        "link": "https://www.javascript.com/"
+    },
+    {
+        "name": "React JS",
+        "className": "reactjs",
+        "link": "https://reactjs.org/"
+    },
+    {
+        "name": "Git",
+        "className": "git",
+        "link": "https://git-scm.com"
+    },
+    {
+        "name": "Github",
+        "className": "github",
+        "link": "https://github.com/"
+    },
+    {
+        "name": "Grunt",
+        "className": "grunt",
+        "link": "https://gruntjs.com/"
+    },
+    {
+        "name": "HTML5",
+        "className": "html5",
+        "link": "https://developer.mozilla.org/en-US/docs/Web/Guide/HTML/HTML5"
+    },
+    {
+        "name": "CSS3",
+        "className": "css3",
+        "link": "https://developer.mozilla.org/en-US/docs/Web/CSS/CSS3"
+    },
+    {
+        "name": "Bootstrap",
+        "className": "bootstrap",
+        "link": "https://getbootstrap.com/"
+    },
+    {
+        "name": "Browserify",
+        "className": "browserify",
+        "link": "http://browserify.org/"
+    },
+    {
+        "name": "NPM",
+        "className": "npm",
+        "link": "https://www.npmjs.com/"
+    },
+    {
+        "name": "SQL",
+        "className": "sql",
+        "link": "https://www.sqlite.org/"
+    },
+    {
+        "name": "SQL Server",
+        "className": "sqlserver",
+        "link": "https://www.microsoft.com/en-us/sql-server/sql-server-2017"
+    },
+    {
+        "name": "Visual Studio Code",
+        "className": "vscode",
+        "link": "https://code.visualstudio.com/"
+    },
+    {
+        "name": "C#",
+        "className": "csharp",
+        "link": "https://docs.microsoft.com/en-us/dotnet/csharp/"
+    },
+    {
+        "name": ".NET Core",
+        "className": "dotnetcore",
+        "link": "https://www.microsoft.com/net/"
+    },
+    {
+        "name": "Visual Studio",
+        "className": "visualstudio",
+        "link": "https://www.visualstudio.com/vs/"
+    },
+    {
+        "name": "ASP .NET MVC",
+        "className": "dotnetmvc",
+        "link": "https://www.asp.net/mvc"
+    }
+]
+
+export default technologies
