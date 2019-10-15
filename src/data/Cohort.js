cd03b65859c95b0736df29f6155d75c22c4a3a68
krystal gates <krystal_sabrina@yahoo.com>
2019-10-15 09:07:34 -0500

    fix data

--- src/data/Cohort.js
+++ src/data/Cohort.js
@@ -20,7 +20,128 @@ const cohort = [
         "email": "shanemiller89@gmail.com",
         "reelThemIn": '"The fool wonders, the wise man asks.” -Benjamin Disraeli',
         "bio": "Software development may be a recent occurrence for me, but has always been a longtime interest. I am a creator and a problem solver. Two skills that come in handy in this industry, and which really became a big draw for me to get into this field. Nothing is more satisfying to me than getting a delete button to delete what you want, instead of, you know, wiping your whole database. My original background was graphic design, but after freelancing, I found out I'd rather be the one creating the interface than designing it, and I couldn't be happier with my choice! "
-    }
+    },
+    {
+        "id": 3,
+        "firstName": "Benjamin",
+        "lastName": "Parker",
+        "portfolio": "",
+        "linkedin": "https://www.linkedin.com/in/benjaminpparker12/",
+        "github": "https://github.com/bparker12",
+        "email": "ben.p.parker12@gmail.com",
+        "reelThemIn": '"You miss 100% of the shots you don’t take" - Wayne Gretzky',
+        "bio": "I am an experienced materials program manager with a demonstrated history working in the Defense & Space industry.I decided to make the move to software engineering because I became interested in automating processes in my previous role. I enjoy the challenge of making things work together for an application. The more I learn the more excited at the possibilities to make something better. It is very exciting to know that no matter how long you will be in the developer field, you will always learn new things. "
+    },
+    {
+        "id": 4,
+        "firstName": "Joe",
+        "lastName": "Kennerly",
+        "portfolio": "https://joekennerly.github.io/",
+        "linkedin": "https://www.linkedin.com/in/joekennerly/",
+        "github": "https://github.com/joekennerly",
+        "email": "joseph.kennerly@gmail.com",
+        "reelThemIn": '“Somewhere, something incredible is waiting to be known.” - Carl Sagan',
+        "bio": "First exposure to manipulating web pages in highschool by embedding HTML elements in an ‘About Me’ section of a myspace profile. Throughout college, programming became the glue for personal multimedia projects. More and more, technology has opened new possibilities by automating workflows. Deconstructing things, learning how they work, then reconstructing them have been the main tools for problem solving real world issues."
+    },
+    {
+        "id": 5,
+        "firstName": "Scott",
+        "lastName": "Silver",
+        "portfolio": "",
+        "linkedin": "https://www.linkedin.com/in/scottsilver47/",
+        "github": "https://github.com/Scott47",
+        "email": "scottsilver47@gmail.com",
+        "reelThemIn": '“Wisdom begins when you understand the difference between ‘That doesn’t make sense’ and ‘I don’t understand’”. - Mary Doria Russell',
+        "bio": "I am a learner, a teacher, and an advocate with passion for problem solving and teamwork. For the last 6 years in Nashville, I have been a middle school math teacher. Before then, I utilized my anthropology and archaeology background as a public health advocate, program coordinator, and researcher. Now, I am immersing myself in opportunities to learn and grow as a software developer and meet new people in the Nashville tech community. I also love maps and data!"
+    },
+    {
+        "id": 6,
+        "firstName": "Jake",
+        "lastName": "Scott",
+        "portfolio": "https://shanemiller89.github.io/",
+        "linkedin": "https://www.linkedin.com/in/jake-scott-a48908189/",
+        "github": "https://github.com/MrNoIce",
+        "email": "bemybanker@gmail.com",
+        "reelThemIn": '“Don’t believe everything you believe”',
+        "bio": "I have always been a mix of creative and analytical and have aspired to find a career where this duality co-exists. As a business owner and fabricator, I was able to explore my creativity and pore over details and strategy, but the small scale auto repair industry left me wanting more. As a software developer, I am now able to bring my analytical and creative mindset together in a harmonious way."
+    },
+    {
+        "id": 7,
+        "firstName": "Matthew",
+        "lastName": "McDevitt",
+        "portfolio": "https://mmcdevitt1997.github.io/personal-site/",
+        "linkedin": "https://www.linkedin.com/in/matthew-mcdevitt/",
+        "github": "https://github.com/mmcdevitt1997",
+        "email": "matthewamcdevitt@gmail.com",
+        "reelThemIn": '"Early to bed and early to rise makes a man healthy, wealthy and wise." - Benjamin Franklin',
+        "bio": "What gets me out of bed in the morning is facing problems and developing solutions. I started out freelance developing WordPress sites for friends and non-profits, and enjoyed these projects. However, after learning about the technical structure, I knew I wanted to build my own apps from the ground up. Nashville Software School has given me the skills to create my own applications and the chance to be a part of the Nashville tech community."
+    },
+    {
+        "id": 8,
+        "firstName": "Sydney",
+        "lastName": "Noh",
+        "portfolio": "https://sydneynoh.dev",
+        "linkedin": "https://www.linkedin.com/in/sydneycnoh/",
+        "github": "https://github.com/simplesnoh",
+        "email": "sydneycnoh@gmail.com",
+        "reelThemIn": '"When we seek to rule only ourselves, we are each of us a king." - Kings of The Wyld',
+        "bio": "A native Oregonian, I managed to live on each coast (Oregon, Texas, DC) before finally settling in Nashville 3 years ago. In a previous life I worked in Television for shows such as Fixer Upper, America's Got Talent, and various crime shows but after 5 years of keeping track of fake severed heads and using the word 'shiplapped' more times than I can count I felt I was being called for something else. For a year I worked as an event manager for the Tennessee Performing Arts Center where I stumbled upon an online class on computer science while on a lunch break. It was love at first sight! I found myself playing around with code more and more during my downtime. When I learned about about NSS I decided to make it official and begin making my passion into my career. I can't wait to see where this new path takes me."
+    },
+    {
+        "id": 9,
+        "firstName": "Sam",
+        "lastName": "Birky",
+        "portfolio": "https://sambirky21.github.io/",
+        "linkedin": "https://www.linkedin.com/in/sambirky/",
+        "github": "https://github.com/sambirky21",
+        "email": "sgbirky@gmail.com",
+        "reelThemIn": 'So, don’t waste your mind on nursery rhymes Or fairy tales of blood and wine. It’s turtles all the way down the line" - Sturgill Simpson',
+        "bio": "Before becoming a software programmer, I was working in sales for a luxury tour operator out of Chicago. My curiosity for software development was sparked by conversations with family and friends in the industry. During my time in Chicago, I often strategized best system practices for my department. I began to help new employees navigate our system effectively. With more understanding of our internal software, I found myself passing along ideas to company leaders to make the user experience easier. I began to see a parallel between making a better user experience in my workplace and the work that developers were doing on a day-to-day basis. From there, the idea of being a part of the industry consumed me, and after some time I had no choice but to embark on this new journey. Software programming enables me to look at problems from a different perspective every day. I enjoy the planning process and seeing a solution to a challenge come to life. As a lifelong learner, software development promises a future that will be incredibly fulfilling."
+    },
+    {
+        "id": 10,
+        "firstName": "Adam",
+        "lastName": "Knowles",
+        "portfolio": "https://adamknowles.github.io/",
+        "linkedin": "https://www.linkedin.com/in/adamcoreyknowles/",
+        "github": "https://github.com/AdamKnowles",
+        "email": "acknowles524@gmail.com",
+        "reelThemIn": '“The only people for me are the mad ones, the ones who are mad to live, mad to talk, mad to be saved, desirous of everything at the same time, the ones who never yawn or say a common place thing, but burn burn burn, like fabulous yellow Roman candles exploding like spiders across the stars”.- Jack Kerouak, On the Road',
+        "bio": "Just a nurse breaking into the software development world. As the story commonly goes, I got into programming while working in a totally separate career, nursing. Nursing was beginning to become draining to me. I loved the problem solving aspect of the career, but the solutions were often out of my control. My two best friends, both software developers, encouraged me to try out software development. Shortly after I left my job as a nurse, I started to buckle down and teach myself Java, and fell in love. I decided to formalize my education and officially make my career transition after enrolling at Nashville Software School. I am excited to see where my new career path takes me!"
+    },
+    {
+        "id": 11,
+        "firstName": "Berkeley",
+        "lastName": "Platte",
+        "portfolio": "https://shanemiller89.github.io/",
+        "linkedin": "https://www.linkedin.com/in/berkeley-platte-41855314a/",
+        "github": "https://github.com/BerkeleyPlatte",
+        "email": "berkeleyplatte989@gmail.com",
+        "reelThemIn": '“Keep back nothing.  Nothing that you have not given away will be really yours.” - C.S. Lewis',
+        "bio": "I come from a heavily music-oriented background, having taken music lessons from an early age and earning a Bachelor of Music degree from Belmont University.  I got into coding this past year when my guitar teacher talked to me about coding in a lesson, about how a programmer's job is, essentially, describing reality to a machine.  Being a long-time private guitar teacher myself, I am sensitive to the deeper understanding required of something in order to each it, and I became excited at the realization that a career in programming would, by necessity, draw me into a deeper understanding of reality.  I consider myself a life-long student, and enjoy the nuances and complexities of software development."
+    },
+    {
+        "id": 12,
+        "firstName": "Mary",
+        "lastName": "West",
+        "portfolio": "https://projects-portfolio-84cfa.firebaseapp.com/",
+        "linkedin": "https://www.linkedin.com/in/mary-west/",
+        "github": "https://github.com/shanemiller89",
+        "email": "https://github.com/westmary48",
+        "reelThemIn": '"Education is the most powerful weapon you can use to change the world." - Nelson Mandela',
+        "bio": "I'm a software developer who uses logic, creativity and passion to solve problems. I try to do the right thing and am looking to use my coding skills for good. I have a background in sales and a degree in psychology.  These skills have given me the ability to understand people and communicate effectively. These skills have translated nicely into web development specifically during planning, collaborating, and when answering or asking questions. In my free time you will find me traveling, playing with my dog, or doing something adventurous outside."
+    },
+    {
+        "id": 13,
+        "firstName": "Jeff",
+        "lastName": "Hill",
+        "portfolio": "https://jeff-hill.github.io/Portfolio-Site/",
+        "linkedin": "https://www.linkedin.com/in/jeff-hill78/",
+        "github": "https://github.com/Jeff-Hill",
+        "email": "jeffhill1978@gmail.com",
+        "reelThemIn": '“The best preparation for tomorrow is doing your best today” - H. Jackson Brown Jr.',
+        "bio": "Ever since I began my 12-year career in digital marketing I have always been fascinated with the web development aspect of bringing digital assets to life. How was it that a developer can take a wire frame, graphic representation, and set of desired outcomes and bring it to life on the web? Making the decision to become a full stack developer has been more rewarding than I ever expected. I'm confident my marketing experience combined with my new skills as a developer will be beneficial to any organization I join."
+    },
 ]
 
 export default cohort
